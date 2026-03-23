1439b0ed6a6bb45d9b8cbeeeb1582522a4b965ba
leofau97 <leonardofaulisi97@gmail.com>
2026-03-23 16:35:41 +0100

    chat.js

--- api/chat.js
+++ api/chat.js
@@ -15,7 +15,7 @@ export default async function handler(req, res) {
 
     const requestBody = {
       contents: contents,
-      generationConfig: { maxOutputTokens: 1500, temperature: 0.7, topP: 0.9 }
+      generationConfig: { maxOutputTokens: 800, temperature: 0.7, topP: 0.9 }
     };
 
     if (system) {
